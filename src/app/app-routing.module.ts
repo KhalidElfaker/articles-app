326f948b8884ace8ec5db7decef74f02b4ea6f4a
KhalidElfaker <khalidelfaker@gmail.com>
2018-10-05 11:08:19 +0100

    adding routes
    setting up the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'product-details', loadChildren: './product-details/product-details.module#ProductDetailsPageModule' },
+  { path: 'add-produit', loadChildren: './add-produit/add-produit.module#AddProduitPageModule' },
+  { path: 'modify-produit', loadChildren: './modify-produit/modify-produit.module#ModifyProduitPageModule' },
 ];
 
 @NgModule({
